e2a2cafdab7b5b05c19af8a73b3761d4426cfdff
Yasin <yasinelbuz@gmail.com>
2026-08-22 21:02:21 +0300

    feat: initialize new Astro project with content collections and blog structure

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Yasin Elbüz';
+export const SITE_DESCRIPTION = 'Ürün geliştiriyor ve öğrendiklerimi paylaşıyorum.';
