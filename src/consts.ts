56665672dce8367cdada84fea4acc62984e74182
Yasin <yasinelbuz@gmail.com>
2026-08-24 15:58:36 +0300

    refactor: overhaul homepage layout and content with updated profile information and technology stack

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Yasin Elbüz';
-export const SITE_DESCRIPTION = 'Ürün geliştiriyor ve öğrendiklerimi paylaşıyorum.';
+export const SITE_TITLE = 'Yasin Elbüz | Yazılım Geliştirici & Frontend Engineer';
+export const SITE_DESCRIPTION = "İstanbul'da yaşayan, Javascript ekosistemi, mobil uygulama ve oyun geliştirme ile ilgilenen yazılım geliştiricisi Yasin Elbüz'ün kişisel blogu ve portfolyosu.";
