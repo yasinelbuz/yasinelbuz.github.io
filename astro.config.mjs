51d4ab3db665d110cdbcb0be0504ececd33aeb7c
Yasin <yasinelbuz@gmail.com>
2026-08-24 15:31:37 +0300

    feat: configure GitHub Actions deployment and update site URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://yasinelbuz.github.io',
 	integrations: [mdx(), sitemap()],
 });
